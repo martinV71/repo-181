acfbeacfc97694df4eee8bccee177a1cd5a2589b
martin v <mov7188@gmail.com>
2023-03-21 20:22:17 -0300

    creando mi  primer repositorio en git

---
+++ script.js
@@ -0,0 +1,63 @@
+// Bucles FOR
+
+// Quiero que me hagas 10 mensajes por consola que digan "Hola mundo"
+
+/* console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo")
+console.log("Hola mundo") 
+console.log("Hola mundo") */
+
+/*for(let i=1; i<=10; i+1){
+    // es muy importante que si o si va punto y coma
+    console.log("la variable i ahora vale: "+i)
+    console.log("hola mundo")
+} */   
+
+
+//Array
+
+const listaDeAlumnos = ["gonzalo","maria","lucas","brenda","pepe"]
+
+/* console.log("buenas noches, mi nombre es" + lista de alumnos[0])
+console.log("buenas noches, mi nombre es" + lista de alumnos[1])
+console.log("buenas noches, mi nombre es" + lista de alumnos[2])
+console.log("buenas noches, mi nombre es" + lista de alumnos[3])*/
+
+//lista de alumnos, que por cada alumno ejecute por consola un mensaje que diga"buenas noches mi nombre es" + nombreAlumno
+
+/*for (let i=0;i<listaDeAlumnos.length; i=i+1){
+    let alumno=listaDeAlumnos[i]
+    console.log("Buenas noches, mi nombre es"+alumno)
+}*/
+
+
+
+/*let nombre="pepe" //ambito global
+if(true){
+    let nombre="maria"//ambito local
+    console.log(nombre)
+}
+console.log(nombre)*/
+
+
+
+// Dado un array de notas, calcular el promedio
+
+/*const notas1erSemestre = [2,3,5,9,6,10,10]
+
+let sumatoria = 0
+
+for(let i = 0; i < notas1erSemestre.length; i = i + 1 ) {
+    sumatoria = sumatoria + notas1erSemestre[i]
+}
+let promedio = sumatoria / notas1erSemestre.length
+
+console.log("el promedio es de" + promedios)*/
+
